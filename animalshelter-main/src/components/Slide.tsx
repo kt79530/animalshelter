9781ad0b61c9dda6c2f1fe9aa8516ae167f05c35
kt79530 <kiteaman82@gmail.com>
2025-07-30 22:32:33 +0900

    Delete animalshelter-main directory

--- animalshelter-main/src/components/Slide.tsx
+++
@@ -1,9 +0,0 @@
-const Slide = () =>{
-    return(
-        <>
-        메인/슬라이드 컴포넌트
-        </>
-    )
-}
-
-export default Slide;
--- animalshelter-main/src/layout/sub/TitleBanner.tsx
+++
@@ -1,9 +0,0 @@
-const TitleBanner = () => {
-    return(
-        <>
-        게시판: 배너에 해당하는 레이아웃
-        </>
-    )
-}
-
-export default TitleBanner;
